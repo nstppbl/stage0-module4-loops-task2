dad6ce8a16a9371f5ba5281b4cef818907495a46
nstppbl <1nstppbl@gmail.com>
2022-09-03 12:59:04 +0500

    initial commit

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int counter = 0;
+
+        while (counter <= printToInclusive) {
+            System.out.println(counter);
+            counter++;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int count = 0;
+        if (power < 0) {
+            System.out.println("too much power");
+        } else {
+            while (count <= power) {
+                System.out.println(count*count);
+                count++;
+            }
+        }
 
     }
 }
