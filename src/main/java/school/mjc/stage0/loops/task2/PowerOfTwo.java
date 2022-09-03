6aaa81813ee3b6f6cefe41bf57392b1b1298b616
nstppbl <1nstppbl@gmail.com>
2022-09-03 13:29:26 +0500

    third commit

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        int count = 0;
+        int count = 1;
         if (power < 0) {
             System.out.println("too much power");
         } else {
