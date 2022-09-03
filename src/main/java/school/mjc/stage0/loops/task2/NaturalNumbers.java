0d9149ce1b6baa0d568c9aa57e2c7d8b644e79f5
nstppbl <1nstppbl@gmail.com>
2022-09-03 13:22:31 +0500

    third commit

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
         int counter = 0;
+        lastPrinted = 20;
         while (counter <= lastPrinted) {
             System.out.println(counter);
             counter++;
