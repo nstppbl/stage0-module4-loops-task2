6cffd8682619cbad2188a8762cad99e8a36f5fb0
nstppbl <1nstppbl@gmail.com>
2022-09-03 13:08:20 +0500

    initial commit

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,11 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int counter = 0;
+        int i = 1, fact = 1;
 
-        while (counter <= printToInclusive) {
-            System.out.println(counter);
-            counter++;
+        while (i <= printToInclusive) {
+            fact = fact * i;
+            System.out.println(fact);
+            i++;
         }
 
     }
